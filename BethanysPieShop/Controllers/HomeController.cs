b9cedf674c97fdafd2b8cd2316bedf2ded3e1c83
Francisco Nieves <franco.t90@gmail.com>
2018-04-13 11:31:27 -0500

    Added navigation

--- BethanysPieShop/Controllers/HomeController.cs
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -16,8 +16,6 @@ namespace BethanysPieShop.Controllers
 
         public IActionResult Index()
         {
-
-
             var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
 
             var homeViewModel = new HomeViewModel()
@@ -27,6 +25,18 @@ namespace BethanysPieShop.Controllers
             };
 
             return View(homeViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+            {
+                return NotFound();
+            }
+
+            return View(pie);
         }
     }
 }
